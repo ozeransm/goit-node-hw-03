54d3657050a06caf2a3d5537ef51e063177fed94
Serhii Ozeran <ozeransm@gmail.com>
2023-09-17 22:24:27 +0300

    del callback func from routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -13,16 +13,16 @@ const removeItem = require('./removeItem');
 const updateItem = require('./updateItem');
 const updateOneFileldItem = require('./updateOneFileldItem');
 
-router.get('/', async (req, res, next) => getAll(req, res, next));
+router.get('/', getAll);
 
-router.get('/:contactId', async (req, res, next) => getById(req, res, next));
+router.get('/:contactId', getById);
 
-router.post('/', validateBody(schemaBody), async (req, res, next) => addItem(req, res, next));
+router.post('/', validateBody(schemaBody), addItem);
 
-router.delete('/:contactId', async (req, res, next) => removeItem(req, res, next));
+router.delete('/:contactId', removeItem);
 
-router.put('/:contactId', [validateParams(schemaParams), validateBody(schemaBodyUpd)], async (req, res, next) => updateItem(req, res, next));
+router.put('/:contactId', [validateParams(schemaParams), validateBody(schemaBodyUpd)], updateItem);
 
-router.patch('/:contactId/favorite', validateBody(schemaEmptyBody, 'missing field favorite'), async (req, res, next) => updateOneFileldItem(req, res, next));
+router.patch('/:contactId/favorite', validateBody(schemaEmptyBody, 'missing field favorite'), updateOneFileldItem);
 
 module.exports = router;
