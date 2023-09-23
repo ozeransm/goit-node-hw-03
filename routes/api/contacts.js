4db7a0bceb06c0201e633a8f1a60c15d766bf643
Serhii Ozeran <ozeransm@gmail.com>
2023-09-23 11:25:18 +0300

    replace rout login register ...  to new file auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,25 +12,25 @@ const addItem = require('../../controlers/addItem');
 const removeItem = require('../../controlers/removeItem');
 const updateItem = require('../../controlers/updateItem');
 const updateOneFileldItem = require('../../controlers/updateOneFileldItem');
-const singup = require('../../auth/user/singup');
-const singin = require('../../auth/user/singin');
+// const singup = require('../../auth/user/singup');
+// const singin = require('../../auth/user/singin');
 const auth = require('../../middlewares/auth');
-const logout = require('../../auth/user/logout');
-const current = require('../../controlers/current');
-const validateSignUp = require('../../middlewares/validateSignUp');
+// const logout = require('../../auth/user/logout');
+// const current = require('../../controlers/current');
+// const validateSignUp = require('../../middlewares/validateSignUp');
 const schemaRegister = require('../../middlewares/schemaRegister');
 const schemaLogin = require('../../middlewares/schemaLogin')
-const validateSignIn = require('../../middlewares/validateSignIn');
+// const validateSignIn = require('../../middlewares/validateSignIn');
 const updateSubscription = require('../../auth/user/updateSubscription');
 const schemaEmptyBodySub = require('../../middlewares/chemaEmptyBodySub');
 
-router.post('/users/register', validateSignUp(schemaRegister), singup);
+// router.post('/users/register', validateSignUp(schemaRegister), singup);
 
-router.post('/users/login', validateSignIn(schemaLogin), singin); 
+// router.post('/users/login', validateSignIn(schemaLogin), singin); 
 
-router.post('/users/logout', auth, logout);
+// router.post('/users/logout', auth, logout);
 
-router.get('/users/current', auth, current);
+// router.get('/users/current', auth, current);
 
 router.get('/', auth, getAll);
 
