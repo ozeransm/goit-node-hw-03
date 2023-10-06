b6c46dfb57dd3a0fd326df75fc8099ad58445538
Serhii Ozeran <ozeransm@gmail.com>
2023-10-07 00:09:50 +0300

    create dockerfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {DB_HOST} = process.env;
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log('Database connection successful');
-    app.listen(3000, () => {
+    app.listen(3001, () => {
       console.log("Server running. Use our API on port: 3000")
     })
   }
