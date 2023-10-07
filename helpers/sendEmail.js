d1a3e81fb7ad46a4a5115486879edd2b49232682
Serhii Ozeran <ozeransm@gmail.com>
2023-10-07 19:35:31 +0300

    update task, replace mail sandgrid to env

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-const {SEND_GRID_API_KEY} = process.env;
+const {SEND_GRID_API_KEY, EMAIL_SEND_GRID} = process.env;
 
 sgMail.setApiKey(SEND_GRID_API_KEY);
 
@@ -7,7 +7,7 @@ const sendEmail = (email, verifyCode) => {
   console.log(verifyCode)
 const msg = {
   to: email,
-  from: 'ozyoz@yahoo.com', // Use the email address or domain you verified above
+  from: EMAIL_SEND_GRID, // Use the email address or domain you verified above
   subject: 'Verify email',
   html: `<h3>Hello! click link for confirm you email <a target="_blank" href="http://www.localhost:3000/api/contacts/users/verify/${verifyCode}">Link</a> </h3>`,
 };
